838de178ddbd4011e784ac86c6c19cca7185ed1e
Davi2004 <davipereirar39@gmail.com>
2024-06-20 08:40:38 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+  // Pegar a tag img
+  var img = document.querySelector("#profile img")
+  // Condição: Se estiver no plano de fundo "LightMode", adicionar a imagem light, caso contrário, usar a imagem normal.
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/avatar-light.png")
+  }else {
+    img.setAttribute("src", "./assets/avatar.png")    
+  }
+  
+}
